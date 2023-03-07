58477d2ac47ea19734ccc5193b936045882f9d67
lee2752 <69233948+leedh2752@users.noreply.github.com>
2023-03-07 23:46:19 +0900

    add: printf

---
+++ src/main/java/Introduction/Ex5.java
@@ -0,0 +1,11 @@
+package Introduction;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        String s = "java";
+        int i = 100;
+        System.out.printf("%-15s%03d%n", s, i);
+//        System.out.printf("%-15s%03d%n",s1,x);
+
+    }
+}
