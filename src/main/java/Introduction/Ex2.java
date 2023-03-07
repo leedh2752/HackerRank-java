6d079b54c58ace280f31b489a32fd68a9709b786
lee2752 <69233948+leedh2752@users.noreply.github.com>
2023-03-07 23:45:42 +0900

    add: printf

---
+++ src/main/java/Introduction/Ex2.java
@@ -0,0 +1,22 @@
+package Introduction;
+
+import java.util.*;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        while(scan.hasNext()) {
+//            System.out.println(scan.nextInt());
+//        }
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.printf("%s\n%s\n%s",a,b,c);
+//        System.out.println(a);
+//        System.out.println(a);
+
+    }
+}
