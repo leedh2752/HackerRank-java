4143b909dbb00d7d7c393ea1b0fd17483ac84774
lee2752 <69233948+leedh2752@users.noreply.github.com>
2023-03-07 23:46:27 +0900

    add: loops 1

---
+++ src/main/java/Introduction/Ex6.java
@@ -0,0 +1,10 @@
+package Introduction;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        int i = 2;
+        for (int j = 1; j <= 10; j++) {
+            System.out.println(i + " x " + j + " = " + i*j);
+        }
+    }
+}
